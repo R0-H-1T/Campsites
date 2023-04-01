39ddd19388dc72d649483cb0880698d22bcadf6a
Rohit Tank <90775575+R0-H-1T@users.noreply.github.com>
2023-04-01 20:09:22 +0530

    added route for updating profile, and filter campgrounds based on user._id

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,9 @@
 const User = require('../models/user');
+const Campground = require('../models/campground');
+
+// var passport = require("passport");
+// var LocalStrategy= require("passport-local");
+// var passportLocalMongoose = require("passport-local-mongoose");
 
 module.exports.renderRegister = (req, res) => {
     res.render('users/register')
@@ -31,6 +36,86 @@ module.exports.login = (req, res) => {
     delete req.session.returnTo;
     res.redirect(redirectUrl);
 }
+
+
+module.exports.yourcampgrounds = async(req, res) => {
+    const person = req.user._id; 
+    if(person){
+        const campgrounds = await Campground.find({author: person});
+        res.render('users/yourcamp', { campgrounds });
+    }
+}
+
+module.exports.showProfile = async(req, res) => {
+    const person = req.user._id; 
+    if (person){   
+        const myProfile = await User.findById(person)
+        res.render('users/myprofile', { myProfile});
+
+    }   
+}
+
+
+
+module.exports.updateProfile = async(req, res) => {
+    try{
+
+       
+        
+        
+        const id  = req.user._id;
+        console.log(id)
+        //console.log({ ...req.body.user1})
+        // const oldUser = await User.findOne({ username: req.body.user1.email })
+        // await oldUser.setPassword(newPassword);
+        // await oldUser.save();
+
+
+        const { email, username } = req.body.user1;
+        console.log(email)
+        console.log(username)
+        try{
+            const person = await User.findByIdAndUpdate(id, { email, username })
+            await person.save()
+        }catch(error){
+            console.log(error)
+            req.flash('error', 'Invlid username or email');
+            return res.redirect('/profile');
+            // console.log(error)
+            // req.flash('success', 'Welcome to YelpCamp!!');
+
+        }
+        
+
+
+        //the password is not getting saved
+        //password not getting uploaded
+        //rest is working
+        //for now the username will get updated but not the password!
+        console.log('Saved')
+        User.findById(id)
+        .then(foundUser => {
+            foundUser.setPassword(req.body.user1.password)
+                .then(() => {
+                    console.log('password changed');
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+        req.flash('success', 'Successfully updated your profile');
+        res.redirect(`/campgrounds`)
+    }catch(e){
+        console.log(e.message)
+        //res.redirect('/campgrounds')
+    }
+
+}
+
 
 module.exports.logout = (req, res) =>{
     req.logOut();
